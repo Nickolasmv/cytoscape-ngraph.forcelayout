dcb3921deb69de310dcc2f6f5efe91545fbd7a37
nickolas <nikolay.svystun@rokitteck.ua>
2016-02-05 17:03:37 +0200

    pushing changes for v 1.0.7 release

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('confver', ['version'], function(){
 
 gulp.task('version', function( next ){
   var now = new Date();
-  version = '1.0.6'//process.env['VERSION'];
+  version = '1.0.7'//process.env['VERSION'];
 
   if( version ){
     done();
@@ -86,13 +86,13 @@ gulp.task('pkgver', ['version'], function(){
 
 gulp.task('push', shell.task([
   'git add -A',
-  'git commit -m "pushing changes for v 1.0.6 release" || echo Nothing to commit',
+  'git commit -m "pushing changes for v 1.0.7 release" || echo Nothing to commit',
   'git push || echo Nothing to push'
 ]));
 
 gulp.task('tag', shell.task([
-  'git tag -a 1.0.6 -m "tagging v1.0.6"',
-  'git push origin 1.0.6'
+  'git tag -a 1.0.7 -m "tagging v1.0.7"',
+  'git push origin 1.0.7'
 ]));
 
 gulp.task('npm', shell.task([
